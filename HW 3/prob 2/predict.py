82fbed3ab8c4cca802858fb1e75a13b1026faa21
subhadarship <35211168+subhadarship@users.noreply.github.com>
2019-11-18 21:17:29 -0500

    minor edits

--- HW 3/prob 2/predict.py
+++ HW 3/prob 2/predict.py
@@ -68,6 +68,7 @@ if __name__ == "__main__":
     criterion = nn.CrossEntropyLoss()
 
     # load checkpoint
+    tqdm.write(f'loading checkpoint {ckpt_path}')
     state = load_checkpoint(ckpt_path, model, device=device)
 
     tqdm.write(
